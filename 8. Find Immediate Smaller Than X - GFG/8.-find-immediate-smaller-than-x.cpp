e198ff8178e038bdd954cecf65d801450db818f1
Anmol Chopra <54865022+anmol201212@users.noreply.github.com>
2022-03-12 19:26:13 +0530

    Added solution - LeetHub

---
+++ 8. Find Immediate Smaller Than X - GFG/8.-find-immediate-smaller-than-x.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // User function to complete
+    // arr[]: input array
+    // n: size of array
+    // k: find element smaller and closest to it. 
+    int immediateSmaller(int arr[], int n, int x)
+    {
+        // your code here
+        int temp[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(x>arr[i]){
+                temp[count] = arr[i];
+                count++;
+            }
+        }
+        if(count==0) return -1;
+        return *max_element(temp,temp+count);
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    
+            int n;
+        	cin >> n;
+        	
+        	int arr[n];
+        	
+        	for(int i = 0;i<n;i++){
+        	    cin >> arr[i];
+        	}
+        	
+        	int x;
+        	cin >> x;
+        	Solution ob;
+        	cout << ob.immediateSmaller(arr, n, x) << endl;
+	}
+	
+	
+	return 0;
+}
+  // } Driver Code Ends
