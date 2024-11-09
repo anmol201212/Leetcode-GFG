6f7e530adcaf0a6c3b2dd7f5c23ea14da0e3e5e2
Anmol Chopra <54865022+anmol201212@users.noreply.github.com>
2024-11-09 21:31:33 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (89.40%) - LeetSync

--- 2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.py
+++ 2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.py
@@ -1,16 +1,18 @@
 class Solution:
     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
-        output=[]
-        if numOnes:
-            for i in range(numOnes):
-                output.append(1)
-        if numZeros:
-            for i in range(numZeros):
-                output.append(0)
-        if numNegOnes:
-            for i in range(numNegOnes):
-                output.append(-1)
-        output.sort(reverse=True)
-        print(output)
-        return sum(output[:k])
-        
+        # output=[]
+        # if numOnes:
+        #     for i in range(numOnes):
+        #         output.append(1)
+        # if numZeros:
+        #     for i in range(numZeros):
+        #         output.append(0)
+        # if numNegOnes:
+        #     for i in range(numNegOnes):
+        #         output.append(-1)
+        # output.sort(reverse=True)
+        # print(output)
+        # return sum(output[:k])
+        if k <= numOnes + numZeros:
+            return min(k, numOnes)
+        return numOnes - (k - (numOnes + numZeros))
