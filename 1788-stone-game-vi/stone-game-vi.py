e83500061eb3ad49f2bc332adce29162b8f4c869
Anmol Chopra <54865022+anmol201212@users.noreply.github.com>
2025-02-22 04:26:58 +0530

    Time: 132 ms (87.83%) | Memory: 31.9 MB (64.55%) - LeetSync

--- 1788-stone-game-vi/stone-game-vi.py
+++ 1788-stone-game-vi/stone-game-vi.py
@@ -1,9 +1,12 @@
 class Solution:
     def stoneGameVI(self, aliceValues: List[int], bobValues: List[int]) -> int:
-        t=list(zip(aliceValues,bobValues))
-        t=sorted(t,key=lambda x: sum(x),reverse=True)
-        al=sum([i[0] for i in t[::2]])
-        bb=sum([i[1] for i in t[1::2]])
+        t = list(zip(aliceValues,bobValues))
+        t = sorted(t, key = lambda x: sum(x),reverse=True)
+        # print(t)
+        al = sum([i[0] for i in t[::2]])
+        # print(al)
+        bb = sum([i[1] for i in t[1::2]])
+        # print(bb)
         if al>bb:
             return 1
         elif al<bb:
